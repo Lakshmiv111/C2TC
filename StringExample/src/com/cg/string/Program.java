5fec5890562a9cd84082bed6d7f783fb5f7f7fcd
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-09-21 18:28:46 +0530

    string

---
+++ StringExample/src/com/cg/string/Program.java
@@ -0,0 +1,26 @@
+package com.cg.string;
+
+public class Program {
+  public static void main(String [] args) {
+	  
+	  String s1 = "Ramayana";
+      String s2 = "Ramayana";
+      String s3 = "ramayana";
+      String s4 = "Mahabharatha";
+	  
+	if(s1.equals(s1)== true) {
+		System.out.println("Strings s1 & s2 are compare and are found to be equal");
+	}
+	if(s1.equals(s1)== true) {
+		System.out.println("Strings s1 & s3 are compare and are found to be equal");
+	}
+	if(s1.equals(s1)== true) {
+		System.out.println("Strings s1 & s4 are compare and are found to be equal");
+	}
+	if(s1.equals(s1)== true) {
+		System.out.println("Strings are compare and are found to be not equal");
+	}
+				  
+			  
+  }
+}
