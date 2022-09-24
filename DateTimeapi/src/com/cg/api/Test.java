37c91ffab74e789a4005c01bc1a15ff2e46fcf4b
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-09-24 15:20:06 +0530

    Date and Time API-java 1.8 version new features

---
+++ DateTimeapi/src/com/cg/api/Test.java
@@ -0,0 +1,15 @@
+package com.cg.api;
+
+import java.time.*;// must and should import is required.
+public class Test 
+{
+	public static void main(String[] args) {
+		LocalDate date = LocalDate.now();// now represents current Date
+		System.out.println(date);
+		
+		LocalTime time = LocalTime.now();// now represents current Time
+		System.out.println(time);
+		
+	}
+
+}
