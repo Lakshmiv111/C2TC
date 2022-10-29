04fbe7916325d8ba6e72020899768a41fbe74f3f
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-10-29 18:31:45 +0530

    ThreadGroup

---
+++ Runnable/src/com/gqt/runnable/ThreadGroupExmp.java
@@ -0,0 +1,28 @@
+package com.gqt.runnable;
+
+public class ThreadGroupExmp implements Runnable {
+   
+	public void run()
+	{
+System.out.println("The Thread that is executing is:"+ Thread.currentThread().getName());
+		
+	}
+}
+class LaunchGroups{
+   public static void main(String[] args) {
+	ThreadGroupExmp tgx = new ThreadGroupExmp();
+	ThreadGroup tg = new ThreadGroup("Example");
+
+	Thread t1 = new Thread(tg,tgx,"Thread-1");
+	Thread t2 = new Thread(tg,tgx,"Thread-2");
+	Thread t3 = new Thread(tg,tgx,"Thread-3");
+
+	t1.start();
+	t2.start();
+	t3.start();
+	
+	tg.list();
+		
+	}
+
+}
