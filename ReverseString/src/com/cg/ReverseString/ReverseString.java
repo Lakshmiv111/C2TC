47d1833cc841bcab561929f31611a0a43b411a76
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-08 13:56:09 +0530

    Reverse String

---
+++ ReverseString/src/com/cg/ReverseString/ReverseString.java
@@ -0,0 +1,16 @@
+package com.cg.ReverseString;
+
+public class ReverseString {
+
+	public static void main(String[] args) {
+		
+		String str = "Lakshmi";
+		String rev = " ";
+		
+		StringBuffer sb = new StringBuffer(str);
+		System.out.println(sb.reverse());
+				
+
+	}
+
+}
