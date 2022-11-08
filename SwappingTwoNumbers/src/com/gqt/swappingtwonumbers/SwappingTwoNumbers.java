9a436d016ac1fcb758b3dd342815cdeb37b28095
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-08 13:02:41 +0530

    Swap Two Numbers.....

---
+++ SwappingTwoNumbers/src/com/gqt/swappingtwonumbers/SwappingTwoNumbers.java
@@ -0,0 +1,19 @@
+package com.gqt.swappingtwonumbers;
+
+public class SwappingTwoNumbers {
+
+	public static void main(String[] args) {
+		
+		int a =10, b=20;
+		
+System.out.println("Before Swapping Two Numbers....." +a+" "+b);
+
+   int t = a;
+   a=b;
+   b = t;
+  
+System.out.println("After Swapping Two Numbers....." +a+" "+b);
+   
+	}
+
+}
