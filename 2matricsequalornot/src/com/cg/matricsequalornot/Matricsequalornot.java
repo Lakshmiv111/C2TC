7adc72078005e682497ca5eaf19d9c4b612f7ab6
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-16 15:41:09 +0530

    2matricsequalornot

---
+++ 2matricsequalornot/src/com/cg/matricsequalornot/Matricsequalornot.java
@@ -0,0 +1,17 @@
+package com.cg.matricsequalornot;
+
+public class Matricsequalornot {
+
+	public static void main(String[] args) {
+		
+		int[]arr= {12,23,34,45,67,78,89,22,33,41,24,25,26,18,98,65};
+		int arrsum = 0;
+		
+		for (int j=0;j<arr.length; j++)
+		{
+			arrsum=arrsum+arr[j];
+		}
+System.out.println(arrsum);
+	}
+
+}
