b7b247ce6b97bd2c4599e2e6f7f2ef782bc4b7a3
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-22 15:52:26 +0530

    EndsWith()methodbuilt_in method

---
+++ EndsWith()methodbuilt_in method/src/com/cg/EndsWithmethod/EndsWithmethod.java
@@ -0,0 +1,13 @@
+package com.cg.EndsWithmethod;
+
+public class EndsWithmethod {
+
+	public static void main(String[] args) {
+		
+		String s1="Java by JavaPoint";
+		System.out.println(s1.endsWith("t"));
+		System.out.println(s1.endsWith("point"));
+
+	}
+
+}
