3c6df20b47cb351d5a42c323bcd9964c1484c4aa
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-22 16:00:59 +0530

    BuiltinMethod_Concat()

---
+++ BuiltinMethod_Concat()/src/com/cg/BuiltinMethod_Concat/BuiltinMethod_Concat.java
@@ -0,0 +1,14 @@
+package com.cg.BuiltinMethod_Concat;
+
+public class BuiltinMethod_Concat {
+
+	public static void main(String[] args) {
+		
+		String s1="Lakshmi";
+		String s2="sri";
+		String s3=s1.concat(s2);
+		System.out.println(s3);
+
+	}
+
+}
