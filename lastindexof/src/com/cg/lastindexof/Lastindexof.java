b3aad19eb1f83ecb0410489068686a1856305cd1
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-22 15:47:24 +0530

    lastindexof

---
+++ lastindexof/src/com/cg/lastindexof/Lastindexof.java
@@ -0,0 +1,13 @@
+package com.cg.lastindexof;
+
+public class Lastindexof {
+
+	public static void main(String[] args) {
+	
+		String s1 = "this is index of example";
+		int index1 = s1.lastIndexOf(s1);
+		System.out.println(index1);
+
+	}
+
+}
