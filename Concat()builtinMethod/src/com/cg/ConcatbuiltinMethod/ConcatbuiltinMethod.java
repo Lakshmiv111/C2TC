1a90caa30d6e86b70a5f85e4112e4ae90c7e7507
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-22 15:56:21 +0530

    Concat()builtinMethod

---
+++ Concat()builtinMethod/src/com/cg/ConcatbuiltinMethod/ConcatbuiltinMethod.java
@@ -0,0 +1,16 @@
+package com.cg.ConcatbuiltinMethod;
+
+public class ConcatbuiltinMethod {
+
+	public static void main(String[] args) {
+		
+	
+		String str1="java";
+		String str2="programming";
+		System.out.println(str1.concat(str2));
+		
+		
+	}
+	
+
+}
