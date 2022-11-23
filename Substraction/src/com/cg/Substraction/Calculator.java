82abf47acec7cc92ec199687492c4add6f79e9c6
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-23 15:13:01 +0530

    WAP to demonstrate method of type1 with method to perform Substraction

---
+++ Substraction/src/com/cg/Substraction/Calculator.java
@@ -0,0 +1,21 @@
+package com.cg.Substraction;
+
+public class Calculator {
+
+	 void sub()
+	 {
+		 int a= 12;
+		 int b=10;
+		 int c=a-b;
+		 System.out.println(c);
+	 }
+}
+  class type3{
+	public static void main(String[] args) {
+
+		Calculator c= new Calculator();
+		c.sub();
+
+	}
+
+}
