dcbdd0a32970c1907963720722967b0722927b3f
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-23 14:03:01 +0530

    WAP to count total number of consonants in the given strings

---
+++ ConsonantsString/src/com/cg/ConsonantsString/ConsonantsString.java
@@ -0,0 +1,35 @@
+package com.cg.ConsonantsString;
+
+import java.util.Scanner;
+
+public class ConsonantsString {
+
+	public static void main(String[] args) {
+		
+	int count=0;
+	System.out.println("Enter the name:");	
+    Scanner sc= new Scanner(System.in);	
+    String sentence= sc.next();
+    sentence = sentence.toLowerCase();
+    
+    for(int i=0; i<sentence.length(); i++)
+    {
+    	char ch=sentence.charAt(i);
+    if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'||ch==' ')
+    {
+    	System.out.println(" ");
+    }
+    else if(ch!= ' ')
+    {
+    	count++;
+    }
+  }
+    
+    System.out.println("total number of consonants:"+count);
+    
+    
+    
+    
+	}
+
+}
