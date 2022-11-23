75b582ae1e009c61b797ecc94b0f2b2880743295
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-23 15:19:03 +0530

    WAP to demonstrate method of type1 with method to perform division

---
+++ Division/src/com/cg/Division/Calculator.java
@@ -0,0 +1,21 @@
+package com.cg.Division;
+
+public class Calculator {
+
+	void div()
+	{
+		int a=5;
+		int b=120;
+		int c=a/b;
+		System.out.println(c);
+	}
+}
+class type3{
+	public static void main(String[] args) {
+	
+		Calculator c= new Calculator();
+		c.div();
+		
+	}
+
+}
