2165d85b753da92b63cd184f0a2ab449f470ad78
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-23 18:21:07 +0530

    Create a 1D array of static type to store your marks and print yourmarks

---
+++ ArrayConcepts/src/com/cg/ArrayConcepts/ArrayConcepts.java
@@ -0,0 +1,22 @@
+package com.cg.ArrayConcepts;
+
+public class ArrayConcepts {
+
+	public static void main(String[] args) {
+		
+		int arr[]= new int[6];
+		arr[0]=86;
+		arr[1]=80;
+		arr[2]=86;
+		arr[3]=88;
+		arr[4]=76;
+		arr[5]=86;
+		for(int i=0; i<6;i++)
+		{
+			//System.out.println(arr[i]);
+			System.out.println("the marks of student"+i +"is:"+arr[i]);
+		}
+		
+	}
+
+}
