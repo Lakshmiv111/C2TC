e18fb32a76f3d9138966a8eced6e260a21040e80
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-23 14:51:36 +0530

    WAP to demonstrate method of type1 with method to perform addtion

---
+++ Addtion/src/com/cg/addtion/Calculator.java
@@ -0,0 +1,22 @@
+package com.cg.addtion;
+
+public class Calculator {
+	
+	void add()
+	{
+		int a=10;
+		int b=200;
+		int c=a+b;
+		System.out.println(c);
+	}
+}
+ class Type1{
+	 public static void main(String[] args) {		
+		
+	Calculator c = new Calculator();
+	c.add();
+		
+		
+	}
+
+}
