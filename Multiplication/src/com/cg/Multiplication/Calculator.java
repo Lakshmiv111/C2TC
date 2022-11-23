bab7a1080ec199f23bffd30ff4ffd708aad5afa0
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-23 15:07:23 +0530

    WAP to demonstrate method of type1 with method to perform multiplication

---
+++ Multiplication/src/com/cg/Multiplication/Calculator.java
@@ -0,0 +1,22 @@
+package com.cg.Multiplication;
+
+public class Calculator {
+
+	void mult()
+	
+	{
+		int a=10;
+		int b=20;
+		int c=a*b;
+		System.out.println(c);
+	}
+	}
+	class type2{
+		public static void main(String[] args) {
+	
+	Calculator	c= new Calculator();
+	c.mult();
+		
+	}
+
+}
