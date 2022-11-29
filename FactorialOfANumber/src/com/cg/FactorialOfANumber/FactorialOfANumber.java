b618e9551efeff1d1d4487499541d8507f726aa4
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-11-29 18:01:03 +0530

    Maximum Elements In An Array

--- FactorialOfANumber/src/com/cg/FactorialOfANumber/FactorialOfANumber.java
+++ FactorialOfANumber/src/com/cg/FactorialOfANumber/FactorialOfANumber.java
@@ -4,7 +4,7 @@ public class FactorialOfANumber {
 
 	public static void main(String[] args) {
 		
-		int num=10;
+/*		int num=10;
 		long factorial = 1;
 		for(int i=1; i<num; i++)
 		{
@@ -14,3 +14,16 @@ public class FactorialOfANumber {
 	}
 
 }
+*/
+		int num=5;
+		long factorial = 1;
+		for(int i=num; i>=1; i--)
+		{
+			factorial=factorial*i;
+		}
+    System.out.println("Factorial of a Number is:" +factorial);
+	}
+
+
+
+	}
