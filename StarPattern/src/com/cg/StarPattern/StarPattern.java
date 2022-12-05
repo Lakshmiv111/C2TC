d8c442436b1e494b9989efe5f8056b9333173405
Lakshmiv111 <106051527+Lakshmiv111@users.noreply.github.com>
2022-12-05 17:29:05 +0530

    Wap to print your name 10 times in java using for loop

---
+++ StarPattern/src/com/cg/StarPattern/StarPattern.java
@@ -0,0 +1,14 @@
+package com.cg.StarPattern;
+
+public class StarPattern {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++)
+		{
+		System.out.println("Lakshmi V");
+		}
+
+	}
+
+}
